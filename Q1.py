aeb325e3bdc4c4ae6ad4c3264120a51b997dc7b2
PG-DAI93 <pgdai93@gmail.com>
2024-07-12 09:52:50 +0530

    .Py file has been committed

---
+++ Q1.py
@@ -0,0 +1,10 @@
+#Taking input of 2 numbers from the user
+n1 = int(input("Enter n1: "))
+n2 = int(input("Enter n2: "))
+
+#Created a function to perform the addition
+def addition(x,y):
+    return x+y
+
+#Displaying the output of the addition
+print(f"Addition of two numbers is: {addition(n1,n2)}")
